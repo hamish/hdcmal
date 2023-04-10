c1903abec26e639abbc633ec126cd70bd9a21290
hamish Currie <hamish@currie.com.au>
2023-04-11 09:15:34 +1000

    cargo fmt

--- impls/hdc/step1_read_print.rs
+++ impls/hdc/step1_read_print.rs
@@ -15,95 +15,28 @@ fn main() -> Result<()> {
     };
     let mut rl: Editor<(), _> = Editor::with_history(config, history)?;
 
-
     loop {
         let readline = rl.readline("user> ");
         match readline {
             Ok(line) => {
-                println!("{}" , line);
+                println!("{}", line);
                 rl.add_history_entry(line.as_str()).unwrap();
                 io::stdout().flush().unwrap();
-            },
+            }
             Err(ReadlineError::Interrupted) => {
                 println!("CTRL-C");
-                break
-            },
+                break;
+            }
             Err(ReadlineError::Eof) => {
                 println!("CTRL-D");
-                break
-            },
+                break;
+            }
             Err(err) => {
                 println!("Error: {:?}", err);
-                break
+                break;
             }
         }
     }
-    #[cfg(feature = "with-file-history")]
-    rl.save_history("history.txt");
+
     Ok(())
 }
-
-
-/*
-
-
-// use std::io;
-use std::io::{self, Write};
-
-
-use rustyline::error::ReadlineError;
-use rustyline::{DefaultEditor, Result};
-const HISTORY_FILENAME: &str = "~/.hdc_history.txt";
-
-fn main() -> Result<()> {
-    // `()` can be used when no completer is required
-    let mut rl = DefaultEditor::new()?;
-    // #[cfg(feature = "with-file-history")]
-    if rl.load_history(HISTORY_FILENAME).is_err() {
-        println!("No previous history.");
-    }
-    loop {
-        let readline = rl.readline(">> ");
-        match readline {
-            Ok(line) => {
-                rl.add_history_entry(line.as_str());
-                print!("{}" , line);
-                io::stdout().flush().unwrap();            
-            },
-            Err(ReadlineError::Interrupted) => {
-                //println!("CTRL-C");
-                break
-            },
-            Err(ReadlineError::Eof) => {
-                // println!("CTRL-D");
-                break
-            },
-            Err(err) => {
-                println!("Error: {:?}", err);
-                break
-            }
-        }
-    }
-    // #[cfg(feature = "with-file-history")]
-    rl.save_history(HISTORY_FILENAME);
-    Ok(())
-}
-
-
-fn main() {  
-    let stdin = io::stdin();
-    let mut buf = String::new();
-    loop {
-        print!("user> ");
-        io::stdout().flush().unwrap();
-        let bytes = stdin.read_line(&mut buf).expect("read will not fail");
-        if bytes == 0 {
-            break;
-        }
-        print!("{}" , buf);
-        io::stdout().flush().unwrap();
-        buf.clear();
-    }
-
-}
-*/
